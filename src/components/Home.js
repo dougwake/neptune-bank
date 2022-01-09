1719a51956f54e7a071b1e8e4b4fc93fdfdd8daf
B Douglas Wake <bdwake@bdwake.com>
2022-01-09 14:53:02 -0500

    cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext'
-import { auth } from '../firebase'
 
 export default function Home() {
 
@@ -12,7 +11,6 @@ export default function Home() {
     console.log(currentUser ? currentUser.email : "no current user")
 
     async function handleLogout() {
-        // setError('')
 
         try {
             await logout()
