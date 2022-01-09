15c957aabb634e3798f8ec6e7b0ebc028b8c2591
B Douglas Wake <bdwake@bdwake.com>
2022-01-09 09:56:51 -0500

    Added Routing to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import BillPay from './BillPay'
+import PageNotFound from './PageNotFound'
 
 export default function App() {
   return (
-    <h1>Neptune Bank</h1>
+    <>
+      <h1>Neptune Bank</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/billpay" element={<BillPay />}></Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   )
 }
---
+++ src/components/BillPay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BillPay() {
+    return (
+        <h1>BillPay</h1>
+    )
+}
